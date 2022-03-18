26d1a4cb5c956d7bd4a5c99315cca9d758d985c6
Matheus eli <matheuselisouzaferreira@hotmail.com>
2022-03-18 11:16:34 -0300

    estilizando componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
-import  FormularioCadastro  from "./components/FormularioCadastro";
+import FormularioCadastro from "./components/FormularioCadastro";
+import './assets/App.css';
+
 class App extends Component {
   render() {
     return (
-      <section>
+      <section className="conteudo">
         <FormularioCadastro />
         <ListaDeNotas />
       </section>
